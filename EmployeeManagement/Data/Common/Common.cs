dadfa197ce3d4ac5906a4c0827bae58efa01e3da
datttrian <datttrian@gmail.com>
2023-10-11 01:44:03 +0000

    Parsing and validating an Azure Table storage connection string

---
+++ EmployeeManagement/Data/Common/Common.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Data.Common
+{
+    public class Common
+    {
+        public static async Task<CloudTable> CreateTableAsync(string tableName)
+        {
+            string connectionString = ""; // Paste your Azure account's unique connection string.
+
+            CloudStorageAccount cloudStorageAccount;
+
+            if(CloudStorageAccount.TryParse(connectionString, out cloudStorageAccount))
+            {
+                Console.WriteLine("Connectionstring is valid");
+            } else
+            {
+                Console.WriteLine("Connectionstring IS NOT valid");
+            }
+
+            return null;
+        }
+    }
+}
--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EmployeeManagement.Data.Common;
+
+Common.CreateTableAsync("test").GetAwaiter().GetResult();
+Console.ReadKey();
